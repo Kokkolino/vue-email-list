0a4e364ab68e6953be889e88c5abe43669992a5e
Francesco C <fran461j@outlook.it>
2022-11-21 19:33:19 +0100

    func. on mounted, v-show length >= 10

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,12 @@ var app = new Vue({
       emails: []
     },
     mounted(){
-      
+      for(let i = 0; i < 10; i++){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response)=>{
+          this.emails.push(response.data.response);
+        })
+      }
+      console.log(this.emails);
     }
-
   })
